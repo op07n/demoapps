96b5e64a0103f39ce1793ed99aee90f67bea21d2
op07n <39182105+op07n@users.noreply.github.com>
2020-01-08 12:12:19 +0100

    Update streamlit_call.py

--- streamlit_call.py
+++ streamlit_call.py
@@ -3,4 +3,6 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["PYTHONPATH=$PYTHONPATH:StreamlitApp/", "streamlit", "run", "StreamlitApp/app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
+    Popen(
+            ["streamlit", "run", "StreamlitApp/app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"],
+            env=dict(os.environ, PYTHONPATH='StreamlitApp/')
